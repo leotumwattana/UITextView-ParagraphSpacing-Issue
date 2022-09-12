ec76ab2dc413bc26fd120a795792a71cd27fde69
Leo Tumwattana <leo.tumwattana@gmail.com>
2022-09-13 04:07:57 +0800

    Initial Commit

---
+++ UITextView-ParagraphSpacing-Issue/UITextView_ParagraphSpacing_IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITextView_ParagraphSpacing_IssueApp.swift
+//  UITextView-ParagraphSpacing-Issue
+//
+//  Created by Leo Tumwattana on 13/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct UITextView_ParagraphSpacing_IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
